604049a9efda682cf588606e21e09e211f51e8f1
Samantha Oliver <soliver@usgs.gov>
2020-05-26 10:01:37 -0500

    get rid of WQP references/code

--- 20_data_munge/src/munge_data_files.R
+++ 20_data_munge/src/munge_data_files.R
@@ -1,107 +1,13 @@
-f_to_c <- function(f) {
-  c <- (f - 32) * (5/9)
-  return(c)
-}
-
-munge_wqp_withdepths <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-  dat <- readRDS(sc_retrieve(in_ind)) %>%
-    filter(!is.na(`ResultMeasure/MeasureUnitCode`)) %>%
-    filter(ActivityMediaName %in% 'Water') %>%
-    filter(!is.na(`ActivityDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ResultDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ActivityBottomDepthHeightMeasure/MeasureValue`)) %>% 
-    filter(ActivityStartDate > as.Date('1800-01-01') & ActivityStartDate < as.Date('2020-01-01'))
-  
-  # create depth column
-  dat <- mutate(dat, sample_depth = case_when(
-    !is.na(`ActivityDepthHeightMeasure/MeasureValue`) ~ `ActivityDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & !is.na(`ResultDepthHeightMeasure/MeasureValue`) ~ `ResultDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityTopDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityBottomDepthHeightMeasure/MeasureValue`
-  ))
-  
-  dat <- mutate(dat, sample_depth_unit_code = case_when(
-    !is.na(`ActivityDepthHeightMeasure/MeasureValue`) ~ `ActivityDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & !is.na(`ResultDepthHeightMeasure/MeasureValue`) ~ `ResultDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityTopDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityBottomDepthHeightMeasure/MeasureUnitCode`
-  ))
-  
-
-  # now reduce to daily measures
-  dat_reduced <- dat %>%
-    filter(grepl('deg C|deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE)) %>%
-    mutate(ResultMeasureValue = ifelse(grepl('deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE),
-                                       f_to_c(ResultMeasureValue), ResultMeasureValue)) %>%
-    filter(ResultMeasureValue > min_value,
-           ResultMeasureValue < max_value) %>%
-    mutate(`ResultMeasure/MeasureUnitCode` = 'deg C')
-    
-  dat_daily <- group_by(dat_reduced, MonitoringLocationIdentifier, ActivityStartDate, sample_depth) %>%
-    summarize(temperature_mean_daily = mean(ResultMeasureValue),
-              activity_start_times = paste(`ActivityStartTime/Time`, collapse = ', '),
-              n_day = n())
-  
-  dat_daily_meta <- select(dat_reduced, -ResultMeasureValue, -`ActivityStartTime/Time`) %>%
-    group_by(MonitoringLocationIdentifier, ActivityStartDate, sample_depth) %>%
-    summarize_all(first)
-  
-  # bring it together
-  dat_daily <- left_join(dat_daily, dat_daily_meta)
-  
-  # save
-  data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(dat_daily, data_file)
-  gd_put(out_ind, data_file)
-    
-}
-
-munge_wqp_withoutdepths <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-  dat <- readRDS(sc_retrieve(in_ind)) %>%
-    select(MonitoringLocationIdentifier, ActivityStartDate, ResultMeasureValue, 
-           `ResultMeasure/MeasureUnitCode`, `ActivityStartTime/Time`, ActivityMediaName, 
-           `ActivityDepthHeightMeasure/MeasureValue`, `ResultDepthHeightMeasure/MeasureValue`, 
-           `ActivityTopDepthHeightMeasure/MeasureValue`, `ActivityBottomDepthHeightMeasure/MeasureValue`) %>%
-    filter(!is.na(`ResultMeasure/MeasureUnitCode`)) %>%
-    filter(ActivityMediaName %in% 'Water') %>%
-    filter(is.na(`ActivityDepthHeightMeasure/MeasureValue`)&
-             is.na(`ResultDepthHeightMeasure/MeasureValue`)&
-             is.na(`ActivityTopDepthHeightMeasure/MeasureValue`)&
-             is.na(`ActivityBottomDepthHeightMeasure/MeasureValue`)) %>%
-    filter(ActivityStartDate > as.Date('1800-01-01') & ActivityStartDate < as.Date('2020-01-01'))
-  
- 
-  # now reduce to daily measures
-  # first clean up obviously bad data
-  dat_reduced <- dat %>%
-    filter(grepl('deg C|deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE)) %>%
-    mutate(ResultMeasureValue = ifelse(grepl('deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE),
-                                       f_to_c(ResultMeasureValue), ResultMeasureValue)) %>%
-    mutate(`ResultMeasure/MeasureUnitCode` = 'deg C') %>%
-    filter(ResultMeasureValue > min_value, ResultMeasureValue < max_value)
-  
-  dat_daily <- group_by(dat_reduced, MonitoringLocationIdentifier, ActivityStartDate) %>%
-    summarize(temperature_mean_daily = mean(ResultMeasureValue),
-              activity_start_times = paste(`ActivityStartTime/Time`, collapse = ', '),
-              n_day = n())
-  
-  # save
-  data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(dat_daily, data_file)
-  gd_put(out_ind, data_file)
-  
-}
-
 munge_nwis_uv <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
   nwis_df <- readRDS(sc_retrieve(in_ind)) %>%
     filter(!is.na(temp_value), !is.na(dateTime)) %>%
-    filter(grepl('A', cd_value, ignore.case = FALSE)) %>%
     filter(temp_value > min_value,
            temp_value < max_value) %>%
     mutate(dateTime = as.Date(dateTime)) %>%
     group_by(site_no, col_name, dateTime) %>%
-    summarize(temperature_mean_daily = round(mean(temp_value), 3), n_day = n())
+    summarize(temperature_mean_daily = round(mean(temp_value), 3), 
+              cd_value = paste0(unique(cd_value), collapse = ','),
+              n_day = n())
   
   # save
   data_file <- scipiper::as_data_file(out_ind)
