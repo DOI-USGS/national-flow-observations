02cde522695035875ad898f2db9c5ef91608ba6d
Samantha Oliver <soliver@usgs.gov>
2020-05-29 07:40:53 -0500

    remove wqp-specific munge stuff

--- 20_data_munge/src/munge_data_files.R
+++ 20_data_munge/src/munge_data_files.R
@@ -12,7 +12,7 @@ munge_nwis_uv <- function(in_ind, min_value, max_value, max_daily_range, out_ind
   # save
   data_file <- scipiper::as_data_file(out_ind)
   saveRDS(nwis_df, data_file)
-  gd_put(out_ind, data_file)
+  gd_put(out_ind)
 }
 
 munge_nwis_dv <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
@@ -25,37 +25,29 @@ munge_nwis_dv <- function(in_ind, min_value, max_value, max_daily_range, out_ind
   # save
   data_file <- scipiper::as_data_file(out_ind)
   saveRDS(dat, data_file)
-  gd_put(out_ind, data_file)
+  gd_put(out_ind)
   
 }
 
-combine_all_dat <- function(wqp_ind, nwis_dv_ind, nwis_uv_ind, out_ind) {
-  wqp <- readRDS(sc_retrieve(wqp_ind)) %>%
-    mutate(date = as.Date(ActivityStartDate), source = 'wqp') %>%
-    select(site_id = MonitoringLocationIdentifier, 
-           date,
-           temp_degC = temperature_mean_daily,
-           n_obs = n_day, source)
+combine_all_dat <- function(nwis_dv_ind, nwis_uv_ind, out_ind) {
   
   nwis_dv <- readRDS(sc_retrieve(nwis_dv_ind)) %>%
     mutate(date = as.Date(dateTime),
-           source = 'nwiw_dv',
-           site_id = paste0('USGS-', site_no)) %>%
-    select(site_id, date, temp_degC = temp_value, source)
+           source = 'nwis_dv') %>%
+    select(site_id = site_no, date, temp_degC = temp_value, source)
   
   nwis_uv <- readRDS(sc_retrieve(nwis_uv_ind)) %>%
     ungroup() %>%
     mutate(date = as.Date(dateTime),
-           source = 'nwiw_uv',
-           site_id = paste0('USGS-', site_no)) %>%
-    select(site_id, date, temp_degC = temperature_mean_daily, n_obs = n_day, source)
+           source = 'nwis_uv') %>%
+    select(site_id = site_no, date, temp_degC = temperature_mean_daily, n_obs = n_day, source)
   
-  all_dat <- bind_rows(wqp, nwis_dv, nwis_uv)
+  all_dat <- bind_rows(nwis_dv, nwis_uv)
     
   # save
   data_file <- scipiper::as_data_file(out_ind)
   saveRDS(all_dat, data_file)
-  gd_put(out_ind, data_file)
+  gd_put(out_ind)
   
 }
   
