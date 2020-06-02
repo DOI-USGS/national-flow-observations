1f60a7e5f59b5d86cb2816678cebbb275e7bf9c3
Platt <lplatt@usgs.gov>
2020-06-02 12:03:35 -0500

    only ran for a subset of gages

--- 10_nwis_pull/src/inventory_nwis.R
+++ 10_nwis_pull/src/inventory_nwis.R
@@ -16,8 +16,14 @@ inventory_nwis <- function(inv_ind, nwis_pull_params, service) {
     all_dat <- rbind(all_dat, sites)
     
   }
+  
   data_file <- scipiper::as_data_file(inv_ind)
   saveRDS(all_dat, data_file)
   s3_put(inv_ind)
   
+}
+
+subset_for_testing <- function(all_dat, n = 5) {
+  set.seed(10)
+  dplyr::sample_n(all_dat, n)
 }
