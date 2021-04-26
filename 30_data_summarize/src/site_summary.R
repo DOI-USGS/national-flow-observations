f7c3bfd920523c52f0d3387c67c70fe3b03b3fe3
wdwatkins <wwatkins@usgs.gov>
2021-04-26 08:51:43 -0700

    fix typo, don't use reduced UV inventory

--- 30_data_summarize/src/site_summary.R
+++ 30_data_summarize/src/site_summary.R
@@ -1,16 +1,11 @@
 #one row per site, including ID, lat/lon, site type, maybe n records from the data pull
 
 summarize_site_observations <- function(target_ind, daily_flow_ind, 
-                                        dv_inventory, uv_inventory) {
+                                        dv_inventory, uv_inventory_ind) {
   
-  daily_flow <- readRDS(sc_retrieve(daily_flow_ind, remake_file = '20_data_munge.yml')) 
-  #daily_flow <- readRDS('20_data_munge/out/daily_flow.rds')
-  #dv_inventory <- readRDS('10_nwis_pull/inout/nwis_dv_inventory.rds')
-  #uv_inventory <- readRDS('10_nwis_pull/inout/nwis_uv_inventory.rds')
-  #dv_inventory <- readRDS(sc_retrieve(dv_inventory_ind, remake_file = '20_data_munge.yml'))
-  #uv_inventory <- readRDS(sc_retrieve(uv_inventory_ind, remake_file = '20_data_munge.yml'))
-  
-  
+  daily_flow <- readRDS(sc_retrieve(daily_flow_ind, remake_file = '20_data_munge.yml'))
+  uv_inventory <- readRDS(sc_retrieve(uv_inventory_ind, remake_file = '10_nwis_pull.yml'))
+
   daily_summary <- daily_flow %>% 
     group_by(site_id, source) %>% 
     summarize(n_days = n(),
@@ -22,11 +17,13 @@ summarize_site_observations <- function(target_ind, daily_flow_ind,
     left_join(dv_inventory, by = c(site_id = 'site_no'))
   
   daily_summary_uv_site_info <- daily_summary %>% 
-    filter(source == 'nwis_vv') %>% 
+    filter(source == 'nwis_uv') %>% 
     left_join(uv_inventory, by = c(site_id = 'site_no'))
   daily_summary_all_site_info <- bind_rows(daily_summary_dv_site_info,
                                            daily_summary_uv_site_info)
+  
   stopifnot(sum(is.na(daily_summary_all_site_info$station_nm)) == 0)
+  stopifnot(all(c('nwis_uv', 'nwis_dv') %in% daily_summary_all_site_info$source))
   
   saveRDS(file = as_data_file(target_ind), object = daily_summary_all_site_info)
   s3_put(target_ind)
